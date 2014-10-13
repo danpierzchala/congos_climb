d7bba4d5f4475d0e8badeaba1c130a0f46a64d72
danpierzchala <dan.pierzchala@gmail.com>
2014-10-13 09:00:33 -0500

    Create spriterPackage.vhd

---
+++ spriterPackage.vhd
@@ -0,0 +1,7 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+PACKAGE array_guySprite is
+TYPE guySprite is array(26 downto 0, 26 downto 0) of integer;
+END array_guySprite;
