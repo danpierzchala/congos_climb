82cdc28dbfd584e31deea1ba6c6f0060b1731b4a
danpierzchala <dan.pierzchala@gmail.com>
2014-10-13 09:00:10 -0500

    Create arrayPackage.vhd

---
+++ arrayPackage.vhd
@@ -0,0 +1,8 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+PACKAGE array_platPos is
+TYPE platArray is array(0 to 7) of std_logic_vector(9 downto 0);
+END array_platPos;
+
